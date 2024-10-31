0f9743ccab161fdd8064ed84e4bff88bda3bca39
abhikkrishnan <abhijithkkrishnan@outlook.com>
2024-10-31 15:49:19 +0530

    Added apachepdfbox basics

--- src/main/java/com/pdf/pdfmani/pdfmaniController.java
+++ src/main/java/com/pdf/pdfmani/pdfmaniController.java
@@ -1,8 +1,96 @@
 package com.pdf.pdfmani;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class pdfmaniController {
+
+    @Autowired
+    private pdfmaniService pdfService;
+
+     @GetMapping("/generate-pdf")
+    public ResponseEntity<byte[]> generatePdf() throws IOException {
+        byte[] pdfContent = pdfService.generatePdf();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", "sample.pdf");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(pdfContent);
+    }
+
+    @GetMapping("/generate-pdf-merged")
+    public ResponseEntity<byte[]> generatePdf_merged() throws IOException {
+        byte[] pdfContent = pdfService.generatePdf_merged();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", "sample.pdf");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(pdfContent);
+    }
+
+    @GetMapping("/generate-lorem-ipsum-pdf")
+    public ResponseEntity<byte[]> generateLoremIpsumPdf() throws IOException {
+        String title = "Sample PDF with Lorem Ipsum Text";
+        
+        List<String> paragraphs = Arrays.asList(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.",
+            "Sed sit amet libero purus. Fusce gravida, arcu et imperdiet ullamcorper, mauris augue bibendum massa, a auctor magna dolor id magna.",
+            "Donec faucibus malesuada turpis, id malesuada magna auctor quis. Vestibulum sit amet magna nec arcu fermentum efficitur.",
+            "Nulla facilisi. Pellentesque non nulla purus. Praesent vehicula urna at lorem ultricies bibendum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet mi. Vestibulum maximus dictum eleifend. Sed hendrerit massa eleifend hendrerit egestas. Nullam nec leo suscipit, maximus libero at, rutrum orci. Fusce fermentum volutpat nulla, eu vulputate ipsum laoreet non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed leo ante, accumsan eu tincidunt sed, interdum eget nisi. Integer tortor lacus, maximus a imperdiet vel, commodo vel enim. In hac habitasse platea dictumst. Praesent ut ipsum vitae dolor consectetur molestie. Vivamus placerat odio nec nulla facilisis commodo." + //
+            "Vestibulum erat est, lobortis sed lacinia eget, pellentesque ac felis. Cras aliquet eros ac nunc volutpat dapibus. Morbi ultrices euismod dui eu lacinia. Pellentesque volutpat vestibulum bibendum. Curabitur consectetur suscipit sollicitudin. Vivamus eget condimentum dui. Maecenas suscipit nisi id lorem blandit bibendum. Sed imperdiet risus eu metus vulputate ultrices. Sed dictum justo quis mi porta, quis aliquam nisi ultrices. Fusce at magna sagittis felis pellentesque laoreet. Nunc tortor lacus, finibus at consectetur in, accumsan in magna. Mauris non ipsum eu orci fringilla molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus." + //
+            "Mauris vitae vehicula leo. Etiam fringilla ante in erat elementum, ac varius turpis ultricies. Pellentesque imperdiet velit vel nisl tristique, non porttitor libero congue. Sed sit amet nibh volutpat, blandit ligula nec, vestibulum quam. Ut vehicula, turpis non cursus interdum, augue metus condimentum metus, eget placerat mauris ex vitae nisl. Sed sit amet tristique nunc. Sed condimentum purus eros, vel lacinia ex eleifend eget. Vivamus at mauris sed dui egestas luctus sed sit amet urna. Fusce in lectus orci. Mauris mattis augue ac hendrerit tristique." + //
+            "Donec eget nisi consectetur, euismod erat quis, egestas tellus. Quisque sit amet semper tellus. Vestibulum at odio viverra metus mattis molestie. Suspendisse fringilla sodales lectus quis posuere. Mauris hendrerit malesuada nisi, vitae scelerisque eros. Sed odio nibh, dapibus quis vulputate quis, ultricies quis dolor. Ut varius mi quis eros egestas suscipit. Phasellus vulputate lectus a tincidunt ultrices." + // 
+            "Nullam nec magna lorem. Sed magna nunc, consequat ut lobortis ac, tempor ut arcu. Nam feugiat pellentesque dolor. Donec at aliquet nisl, quis egestas risus. Praesent imperdiet dui in neque tristique, congue dapibus dui feugiat. Cras metus magna, lacinia vel lorem at, rutrum maximus mauris. Ut tellus purus, auctor ut efficitur quis, sollicitudin non neque. Aliquam in enim mauris. Donec auctor tristique pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pretium luctus tellus id malesuada. Maecenas nec pretium diam, eget fringilla turpis."+
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget aliquet mi. Vestibulum maximus dictum eleifend. Sed hendrerit massa eleifend hendrerit egestas. Nullam nec leo suscipit, maximus libero at, rutrum orci. Fusce fermentum volutpat nulla, eu vulputate ipsum laoreet non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed leo ante, accumsan eu tincidunt sed, interdum eget nisi. Integer tortor lacus, maximus a imperdiet vel, commodo vel enim. In hac habitasse platea dictumst. Praesent ut ipsum vitae dolor consectetur molestie. Vivamus placerat odio nec nulla facilisis commodo." + //
+                               
+                                "Vestibulum erat est, lobortis sed lacinia eget, pellentesque ac felis. Cras aliquet eros ac nunc volutpat dapibus. Morbi ultrices euismod dui eu lacinia. Pellentesque volutpat vestibulum bibendum. Curabitur consectetur suscipit sollicitudin. Vivamus eget condimentum dui. Maecenas suscipit nisi id lorem blandit bibendum. Sed imperdiet risus eu metus vulputate ultrices. Sed dictum justo quis mi porta, quis aliquam nisi ultrices. Fusce at magna sagittis felis pellentesque laoreet. Nunc tortor lacus, finibus at consectetur in, accumsan in magna. Mauris non ipsum eu orci fringilla molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus." + //
+                               
+                                "Mauris vitae vehicula leo. Etiam fringilla ante in erat elementum, ac varius turpis ultricies. Pellentesque imperdiet velit vel nisl tristique, non porttitor libero congue. Sed sit amet nibh volutpat, blandit ligula nec, vestibulum quam. Ut vehicula, turpis non cursus interdum, augue metus condimentum metus, eget placerat mauris ex vitae nisl. Sed sit amet tristique nunc. Sed condimentum purus eros, vel lacinia ex eleifend eget. Vivamus at mauris sed dui egestas luctus sed sit amet urna. Fusce in lectus orci. Mauris mattis augue ac hendrerit tristique." + //
+                               
+                                "Donec eget nisi consectetur, euismod erat quis, egestas tellus. Quisque sit amet semper tellus. Vestibulum at odio viverra metus mattis molestie. Suspendisse fringilla sodales lectus quis posuere. Mauris hendrerit malesuada nisi, vitae scelerisque eros. Sed odio nibh, dapibus quis vulputate quis, ultricies quis dolor. Ut varius mi quis eros egestas suscipit. Phasellus vulputate lectus a tincidunt ultrices." + //
+                               
+                                "Nullam nec magna lorem. Sed magna nunc, consequat ut lobortis ac, tempor ut arcu. Nam feugiat pellentesque dolor. Donec at aliquet nisl, quis egestas risus. Praesent imperdiet dui in neque tristique, congue dapibus dui feugiat. Cras metus magna, lacinia vel lorem at, rutrum maximus mauris. Ut tellus purus, auctor ut efficitur quis, sollicitudin non neque. Aliquam in enim mauris. Donec auctor tristique pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pretium luctus tellus id malesuada. Maecenas nec pretium diam, eget fringilla turpis."
+            );
+
+        byte[] pdfContent = pdfService.generatePdfWithTitleAndParagraphs(title, paragraphs);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", "lorem_ipsum.pdf");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(pdfContent);
+    }
+
+    @GetMapping("/get-pdf-page")
+    public ResponseEntity<byte[]> get_pdf_page(@RequestParam Integer pageIndex) throws IOException {
+        byte[] pdfContent = pdfService.generatePdfWithSpecificPage(pageIndex-1);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", "sample.pdf");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(pdfContent);
+    }
     
 }
--- src/main/java/com/pdf/pdfmani/pdfmaniService.java
+++ src/main/java/com/pdf/pdfmani/pdfmaniService.java
@@ -1,10 +1,189 @@
 package com.pdf.pdfmani;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.stereotype.Service;
 
 @Service
 public class pdfmaniService {
+
+    private static final float MARGIN = 50;
+    private static final float LINE_HEIGHT = 14;
+    private static final int TITLE_FONT_SIZE = 16;
+    private static final int FONT_SIZE = 12;
+
+    public byte[] generatePdf() throws IOException {
+        try (PDDocument document = new PDDocument()) {
+            // Create a blank page
+            PDPage page = new PDPage();
+            document.addPage(page);
+
+            // Content stream to write content to the page
+            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                contentStream.beginText();
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+                contentStream.newLineAtOffset(100, 700);
+                contentStream.showText("Hello, PDFBox in Spring Boot!");
+                contentStream.endText();
+            }
+
+            // Save PDF to ByteArrayOutputStream to return as byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            document.save(baos);
+            return baos.toByteArray();
+        }
+    }
+
+    public byte[] generatePdfWithTitleAndParagraphs(String title, List<String> paragraphs) throws IOException {
+        try (PDDocument document = new PDDocument()) {
+            PDPage page = new PDPage();
+            document.addPage(page);
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+            try {
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, TITLE_FONT_SIZE);
+
+                // Title
+                float startY = page.getMediaBox().getHeight() - MARGIN;
+                contentStream.beginText();
+                contentStream.newLineAtOffset(MARGIN, startY);
+                contentStream.showText(title);
+                contentStream.endText();
+
+                // Move down after title
+                float currentY = startY - (2 * LINE_HEIGHT);
+
+                contentStream.setFont(PDType1Font.HELVETICA, FONT_SIZE);
+
+                // Handle each paragraph
+                for (String paragraph : paragraphs) {
+                    List<String> lines = wrapTextToWidth(paragraph, PDType1Font.HELVETICA, FONT_SIZE, page.getMediaBox().getWidth() - 2 * MARGIN);
+
+                    for (String line : lines) {
+                        if (currentY < MARGIN + LINE_HEIGHT) {
+                            // Close current content stream and add a new page if not enough space
+                            contentStream.close();
+
+                            page = new PDPage();
+                            document.addPage(page);
+                            contentStream = new PDPageContentStream(document, page);
+                            contentStream.setFont(PDType1Font.HELVETICA, FONT_SIZE);
+                            currentY = page.getMediaBox().getHeight() - MARGIN;
+                        }
+
+                        contentStream.beginText();
+                        contentStream.newLineAtOffset(MARGIN, currentY);
+                        contentStream.showText(line);
+                        contentStream.endText();
+                        currentY -= LINE_HEIGHT;
+                    }
+
+                    // Add space between paragraphs
+                    currentY -= LINE_HEIGHT;
+                }
+            } finally {
+                contentStream.close();
+            }
+
+            // Save PDF to ByteArrayOutputStream to return as byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            document.save(baos);
+            return baos.toByteArray();
+        }
+    }
+
+    /**
+     * Utility method to wrap text to the specified width.
+     */
+    private List<String> wrapTextToWidth(String text, PDType1Font font, int fontSize, float maxWidth) throws IOException {
+        List<String> lines = new ArrayList<>();
+        String[] words = text.split(" ");
+        StringBuilder line = new StringBuilder();
+
+        for (String word : words) {
+            String testLine = line + (line.length() > 0 ? " " : "") + word;
+            float textWidth = font.getStringWidth(testLine) / 1000 * fontSize;
+
+            if (textWidth > maxWidth) {
+                lines.add(line.toString());
+                line = new StringBuilder(word);
+            } else {
+                line.append((line.length() > 0 ? " " : "")).append(word);
+            }
+        }
+
+        if (line.length() > 0) {
+            lines.add(line.toString());
+        }
+
+        return lines;
+    }  
+
+    public byte[] generatePdf_merged() throws IOException {
+        // Path to your existing PDF file
+        String existingPdfPath = "src/main/resources/existing.pdf";
+        
+        try (PDDocument document = new PDDocument()) {
+            // Create a blank page
+            PDPage page = new PDPage();
+            document.addPage(page);
     
+            // Content stream to write content to the blank page
+            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                contentStream.beginText();
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+                contentStream.newLineAtOffset(100, 700);
+                contentStream.showText("Hello, PDFBox in Spring Boot!");
+                contentStream.endText();
+            }
+    
+            // Load the existing PDF
+            try (PDDocument existingDocument = PDDocument.load(new File(existingPdfPath))) {
+                PDFMergerUtility merger = new PDFMergerUtility();
+                merger.appendDocument(document, existingDocument);
+            }
+    
+            // Save PDF to ByteArrayOutputStream to return as byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            document.save(baos);
+            return baos.toByteArray();
+        }
+    }    
+
+    public byte[] generatePdfWithSpecificPage(int pageIndex) throws IOException {
+        // Path to your existing PDF file
+        String existingPdfPath = "src/main/resources/existing.pdf";
+        
+        try (PDDocument document = new PDDocument();
+             PDDocument existingDocument = PDDocument.load(new File(existingPdfPath))) {
+            
+
+                
+    
+            // Get the specific page from the existing document
+            if (pageIndex < existingDocument.getNumberOfPages()) {
+                PDPage specificPage = existingDocument.getPage(pageIndex);
+                document.addPage(specificPage);
+            } else {
+                throw new IllegalArgumentException("Invalid page index: " + pageIndex);
+            }
+    
+            // Save PDF to ByteArrayOutputStream to return as byte array
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            document.save(baos);
+            return baos.toByteArray();
+        }
+    }
 }
 
 
